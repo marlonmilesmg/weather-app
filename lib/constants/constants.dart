e5ed1dcddf5cb50edb0dbbd6a390800e4f22c755
marlonmilesmg <magonjomarlon@gmail.com>
2022-07-22 18:53:04 +0200

    constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const aTemperatureTextStyle = TextStyle(
+  fontFamily: '',
+  fontSize: 80.0,
+);
+
+const aConditionTextStyle = TextStyle(
+  fontFamily: '',
+  fontSize: 80.0,
+);
+
+const aMessageTextStyle = TextStyle(
+  fontFamily: '',
+  fontSize: 60.0,
+);
